175e21e0b92734e2f686e2e68172ea341fef394f
Chipper Conley <calumon0@gmail.com>
2022-02-16 07:16:57 -0500

    Add files via upload

--- RPS.c
+++ RPS.c
@@ -12,6 +12,7 @@ char* onThree(){
 //Generates a random choice from the computer. It returns this as a char*, not a char[].
 int randomGen = 0;
 char robotRandArray[1];
+//char* robotRandCharPointer = (char* )malloc(20 * sizeof(char));
 char* robotRandCharPointer;
 robotRandCharPointer = robotRandArray;
 srand(time(0));
@@ -26,10 +27,13 @@ else{
   strncpy(robotRandArray, "S", 1);
 }
 return robotRandCharPointer;
+//free(robotRandCharPointer);
 }
 
 
 int outcome (char* userchoice, char* botchoice){
+//userchoice  = (char* )malloc(20 * sizeof(char));
+//botchoice  = (char* )malloc(20 *sizeof(char));
   //Returns a 0 or 1 to add to the current score, based on the comparisons of the player and computer choices. this also contains a function for changing the highscore back
   //to 1 via overwriting the source file for said score.
   int result = 0;
@@ -60,6 +64,8 @@ int outcome (char* userchoice, char* botchoice){
     result = 0;
   }
   return result;
+//free(userchoice);
+//free(botchoice);
 }
 
 
@@ -83,16 +89,18 @@ int main(){
       scanf("%s", user);
     }
 
+    //char* opponent  = (char*)malloc(20 * sizeof(char));
     char* opponent;
     char Pika[] = {"R"};
     //char Pika[] = {""};
-    //opponent = Pika;
+    opponent = Pika;
     //strcpy(Pika,onThree());//This is where the garbage text comes from
     //strcpy(Pika,"R"); //after this line, user loses it's value?
     //printf("User is %s.\n", user);
     currentscore += outcome(user,opponent);
     printf("The current score is %d.\n", currentscore);
   }
+  //free(opponent);
   if(currentscore > hs){
     hs = currentscore;
     fp = fopen("C:/Users/calum/Desktop/RPS Sim/highscoresourcefile.txt", "w+");
@@ -102,3 +110,44 @@ int main(){
     printf("The high score is %d.\n", hs);
 }
 }
+
+/*
+char str[1024] = "Hello World";
+char tmp[4] = ".";
+
+void append(char* s, char c) {
+        int len = strlen(s);
+        s[len] = c;
+        s[len+1] = '\0';
+}
+
+append(str, tmp);
+
+char opponent[9] = " ";
+*/
+/*
+void onThree(char* opponentchoice){
+opponentchoice = malloc(sizeof(char));
+int randomGen = 0;
+char robotRandArray[1];
+
+srand(time(0));
+randomGen = rand()%3;
+
+//char Rock[1] = "R";
+//char Paper[1] = "P";
+//char Scissors[1] = "S";
+
+if(randomGen == 0){
+  strncpy(opponentchoice , "R", 1);
+}
+else if(randomGen == 1){
+  strncpy(opponentchoice , "P", 1);
+}
+else{
+  strncpy(opponentchoice , "S", 1);
+}
+//opponentchoice = Rock;
+//since this is a pointer, you don't have to return a value to change anything. Remember this technique!
+}
+*/
