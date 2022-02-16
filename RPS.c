03854e9f05b88104f74728442ab19493bd1860aa
Chipper Conley <calumon0@gmail.com>
2022-02-15 20:16:07 -0500

    Add files via upload
    Added commented code for testing. This should solve the issue with the onThree() method. This also contains syntax to manage memory.

--- RPS.c
+++ RPS.c
@@ -12,7 +12,7 @@ char* onThree(){
 //Generates a random choice from the computer. It returns this as a char*, not a char[].
 int randomGen = 0;
 char robotRandArray[1];
-char* robotRandCharPointer;
+char* robotRandCharPointer = malloc(sizeof(char));
 robotRandCharPointer = robotRandArray;
 srand(time(0));
 randomGen = rand()%3;
@@ -26,10 +26,13 @@ else{
   strncpy(robotRandArray, "S", 1);
 }
 return robotRandCharPointer;
+free(robotRandCharPointer);
 }
 
 
 int outcome (char* userchoice, char* botchoice){
+userchoice  = malloc(sizeof(char));
+botchoice  = malloc(sizeof(char));
   //Returns a 0 or 1 to add to the current score, based on the comparisons of the player and computer choices. this also contains a function for changing the highscore back
   //to 1 via overwriting the source file for said score.
   int result = 0;
@@ -60,6 +63,8 @@ int outcome (char* userchoice, char* botchoice){
     result = 0;
   }
   return result;
+free(userchoice);
+free(botchoice);
 }
 
 
@@ -83,10 +88,10 @@ int main(){
       scanf("%s", user);
     }
 
-    char* opponent;
+    char* opponent  = malloc(sizeof(char));
     char Pika[] = {"R"};
     //char Pika[] = {""};
-    opponent = Pika;
+    //opponent = Pika;
     //strcpy(Pika,onThree());//This is where the garbage text comes from
     //strcpy(Pika,"R"); //after this line, user loses it's value?
     //printf("User is %s.\n", user);
@@ -101,4 +106,46 @@ int main(){
   else{
     printf("The high score is %d.\n", hs);
 }
+free(opponent);
 }
+
+/*
+char str[1024] = "Hello World";
+char tmp[4] = ".";
+
+void append(char* s, char c) {
+        int len = strlen(s);
+        s[len] = c;
+        s[len+1] = '\0';
+}
+
+append(str, tmp);
+
+char opponent[9] = " ";
+*/
+/*
+void onThree(char* opponentchoice){
+opponentchoice = malloc(sizeof(char));
+int randomGen = 0;
+char robotRandArray[1];
+
+srand(time(0));
+randomGen = rand()%3;
+
+//char Rock[1] = "R";
+//char Paper[1] = "P";
+//char Scissors[1] = "S";
+
+if(randomGen == 0){
+  strncpy(opponentchoice , "R", 1);
+}
+else if(randomGen == 1){
+  strncpy(opponentchoice , "P", 1);
+}
+else{
+  strncpy(opponentchoice , "S", 1);
+}
+//opponentchoice = Rock;
+//since this is a pointer, you don't have to return a value to change anything. Remember this technique!
+}
+*/
