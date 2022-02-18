2087b9a90102ec30412e869c98f00d0b1a5dbfb7
Chipper Conley <calumon0@gmail.com>
2022-02-18 07:31:42 -0500

    Add files via upload
    Fixed file paths. Added new version of onThree that will pass by reference instead of returning a value. However, for some reason it is putting the userchoice with it.

--- RPS.c
+++ RPS.c
@@ -9,40 +9,36 @@ FILE *fp;
 int *highscore;
 
 void onThree(char* opponentchoice){
+//randomly generates a num 1-3 and sets the string passed in to R P or S.
 //opponentchoice = malloc(sizeof(char));
 int randomGen = 0;
-//char robotRandArray[1];
-
 srand(time(0));
 randomGen = rand()%3;
 
-//char Rock[1] = "R";
-//char Paper[1] = "P";
-//char Scissors[1] = "S";
-
 if(randomGen == 0){
-  strncpy(opponentchoice , "R", 1);
+  strncpy(opponentchoice , "R", 3);
 }
 else if(randomGen == 1){
-  strncpy(opponentchoice , "P", 1);
+  strncpy(opponentchoice , "P", 3);
 }
 else{
-  strncpy(opponentchoice , "S", 1);
+  strncpy(opponentchoice , "S", 3);
 }
-printf("This is what was randomly generated. %s\n", opponentchoice);
-//opponentchoice = Rock;
+printf("Opponent chooses: %s\n", opponentchoice);
 //since this is a pointer, you don't have to return a value to change anything. Remember this technique!
 }
 
 int outcome (char* userchoice, char* botchoice){
   //userchoice  = (char* )malloc(sizeof(char));
   //botchoice  = (char* )malloc(sizeof(char));
+
   //Returns a 0 or 1 to add to the current score, based on the comparisons of the player and computer choices. this also contains a function for changing the highscore back
   //to 1 via overwriting the source file for said score.
+
   int result = 0;
   if(strcmp(userchoice,"Z") == 0){
     printf("Reseting high score to default.........");
-    fp = fopen("C:/Users/calum/Desktop/RPS Sim/highscoresourcefile.txt", "w+");
+    fp = fopen("C:/Users/calum/Desktop/RPS Simulator/highscoresourcefile.txt", "w+");
     fprintf(fp, "1");
     *highscore = 1;
     exit(0);
@@ -82,8 +78,8 @@ int main(){
   int hs = atoi(txtcontents);//atoi coinverts a string/char array to int or returns an error.
   highscore = &hs;
   int currentscore = 0;
-
   printf("Score to beat is %d!\n", hs);
+
   for(int i = 0; i < 3; i++){
     char user[1] = {""};
     printf("%s\n", user);
@@ -93,13 +89,13 @@ int main(){
     //char Pika[] = {"R"};
     char Pika[] = {""};
     opponent = Pika;
-    onThree(Pika);//This is where the garbage text comes from
+    onThree(Pika);
     while (strcmp(user,"R") != 0 && strcmp(user,"P") != 0 && strcmp(user,"S") != 0 && strcmp(user,"Z") != 0){
-      printf("Type in your choice. ");
+      printf("Type in your choice.\n");
       scanf("%s", user);
     }
 
-    //printf("Opponent is %s.\n", opponent);
+    printf("Opponent is %s.\n", opponent);
 
     currentscore += outcome(user,opponent);
     printf("The current score is %d.\n", currentscore);
@@ -107,7 +103,7 @@ int main(){
   //free(opponent);
   if(currentscore > hs){
     hs = currentscore;
-    fp = fopen("C:/Users/calum/Desktop/RPS Sim/highscoresourcefile.txt", "w+");
+    fp = fopen("C:/Users/calum/Desktop/RPS Simulator/highscoresourcefile.txt", "w+");
     fprintf(fp, "%d", hs);
     printf("New high score!");
   }
@@ -116,20 +112,7 @@ int main(){
 }
 }
 
-/*
-char str[1024] = "Hello World";
-char tmp[4] = ".";
 
-void append(char* s, char c) {
-        int len = strlen(s);
-        s[len] = c;
-        s[len+1] = '\0';
-}
-
-append(str, tmp);
-
-char opponent[9] = " ";
-*/
 /*
 void onThree(char* opponentchoice){
 opponentchoice = malloc(sizeof(char));
